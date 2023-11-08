e9b3840a3f81316ac227797e664001b37cd5d329
Dani <120296813+dane118@users.noreply.github.com>
2023-11-08 23:39:25 +0700

    First commit
    Реализовано подключение к БД в классе Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/my_db";
+        String username = "bestuser";
+        String password = "bestuser";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+
         // реализуйте алгоритм здесь
     }
 }
