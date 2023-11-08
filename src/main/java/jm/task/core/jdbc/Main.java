b7d56a21f4e1a8cd9a19afd1da74eefb0848e3b7
Dani <120296813+dane118@users.noreply.github.com>
2023-11-08 23:53:24 +0700

    Добавлено подключение к БД в классе Util

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+/**
+ * Алгоритм работы приложения:
+ * В методе main класса Main должны происходить следующие операции:
+ * Создание таблицы User(ов)
+ * Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления
+ * должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+ * Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+ * Очистка таблицы User(ов)
+ * Удаление таблицы
+ */
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -10,7 +22,7 @@ public class Main {
         String username = "bestuser";
         String password = "bestuser";
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        try (Connection connection = Util.openConnection()) {
             System.out.println(connection.getTransactionIsolation());
         }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/my_db";
+    private static final String USERNAME = "bestuser";
+    private static final String PASSWORD = "bestuser";
+
+    private Util() {
+    }
+
+    public static Connection openConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     // реализуйте настройку соеденения с БД
 }
